4aa9df4b1cb2b724f4c86950502ff9f40db5bcfa
Santiago Vanegas T <santiagov.050506@gmail.com>
2025-03-15 23:12:38 -0500

    Agregado interfaz visual base (GameV.java y GameView.fxml), main modificado, e imagen de icono

--- src/main/java/com/example/sesion4/Main.java
+++ src/main/java/com/example/sesion4/Main.java
@@ -1,13 +1,18 @@
 package com.example.sesion4;
 
+import com.example.sesion4.view.GameV;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args); 
     }
+
+    @Override
     public void start(Stage primaryStage) {
+        GameV gameView = new GameV();
     }
 }
+
---
+++ src/main/java/com/example/sesion4/view/GameV.java
@@ -0,0 +1,35 @@
+package com.example.sesion4.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.io.IOException;
+
+public class GameV extends Stage { 
+
+    private Scene scene;
+
+    public GameV() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/sesion4/view/GameView.fxml"));
+            Parent root = loader.load();
+            scene = new Scene(root);
+            initStage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initStage() {
+        setTitle("Sudoku");
+        setResizable(false);
+
+        getIcons().add(new Image(String.valueOf(getClass().getResource("/com/example/sesion4/icon.png"))));
+
+        setScene(scene);
+        show();
+    }
+}
