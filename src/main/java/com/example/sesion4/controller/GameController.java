61ea2a5f8307eaad001e8b72b3e5abcd3ad8ca8c
Santiago Vanegas T <santiagov.050506@gmail.com>
2025-04-01 22:42:32 -0500

    añadido gridpane ids

--- src/main/java/com/example/sesion4/controller/GameController.java
+++ src/main/java/com/example/sesion4/controller/GameController.java
@@ -13,47 +13,13 @@ import com.example.sesion4.view.PopupWindow;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class GameController {
+
+    @FXML private GridPane sudokuGrid;
     private TextField[][] cells = new TextField[6][6];
     private Listener.CellInfo[][] cellInfos = new Listener.CellInfo[6][6];
-
-    @FXML private TextField cell1;
-    @FXML private TextField cell2;
-    @FXML private TextField cell3;
-    @FXML private TextField cell4;
-    @FXML private TextField cell5;
-    @FXML private TextField cell6;
-    @FXML private TextField cell7;
-    @FXML private TextField cell8;
-    @FXML private TextField cell9;
-    @FXML private TextField cell10;
-    @FXML private TextField cell11;
-    @FXML private TextField cell12;
-    @FXML private TextField cell13;
-    @FXML private TextField cell14;
-    @FXML private TextField cell15;
-    @FXML private TextField cell16;
-    @FXML private TextField cell17;
-    @FXML private TextField cell18;
-    @FXML private TextField cell19;
-    @FXML private TextField cell20;
-    @FXML private TextField cell21;
-    @FXML private TextField cell22;
-    @FXML private TextField cell23;
-    @FXML private TextField cell24;
-    @FXML private TextField cell25;
-    @FXML private TextField cell26;
-    @FXML private TextField cell27;
-    @FXML private TextField cell28;
-    @FXML private TextField cell29;
-    @FXML private TextField cell30;
-    @FXML private TextField cell31;
-    @FXML private TextField cell32;
-    @FXML private TextField cell33;
-    @FXML private TextField cell34;
-    @FXML private TextField cell35;
-    @FXML private TextField cell36;
 
     private GameV view;
     private SudokuLogic sudokuLogic;
@@ -69,60 +35,24 @@ public class GameController {
     public void initialize() {
         SudokuGenerator sudokuGenerator = new SudokuGenerator();
         VerifySudoku verifySudoku = new VerifySudoku();
-        initializeCellsMatrix();
-        this.sudokuLogic = new SudokuAdapter(sudokuGenerator,verifySudoku);
+        this.sudokuLogic = new SudokuAdapter(sudokuGenerator, verifySudoku);
         sudokuBoard = sudokuLogic.generateBoard();
+        
+        initializeCellsMatrix();
         viewUpdateInit();
     }
 
     private void initializeCellsMatrix() {
-        cells[0][0] = cell1;
-        cells[0][1] = cell2;
-        cells[0][2] = cell7;
-        cells[1][0] = cell3;
-        cells[1][1] = cell4;
-        cells[1][2] = cell9;
-        cells[0][3] = cell8;
-        cells[0][4] = cell13;
-        cells[0][5] = cell14;
-        cells[1][3] = cell10;
-        cells[1][4] = cell15;
-        cells[1][5] = cell16;
-
-        cells[2][0] = cell5;
-        cells[2][1] = cell6;
-        cells[2][2] = cell11;
-        cells[3][0] = cell19;
-        cells[3][1] = cell20;
-        cells[3][2] = cell25;
-
-        cells[2][3] = cell12;
-        cells[2][4] = cell17;
-        cells[2][5] = cell18;
-        cells[3][3] = cell26;
-        cells[3][4] = cell31;
-        cells[3][5] = cell32;
-    
-        cells[4][0] = cell21;
-        cells[4][1] = cell22;
-        cells[4][2] = cell27;
-        cells[5][0] = cell23;
-        cells[5][1] = cell24;
-        cells[5][2] = cell29;
-    
-        cells[4][3] = cell28;
-        cells[4][4] = cell33;
-        cells[4][5] = cell34;
-        cells[5][3] = cell30;
-        cells[5][4] = cell35;
-        cells[5][5] = cell36;
-
+        int contador = 1;
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
+                String cellId = "cell" + contador;
+                cells[i][j] = (TextField) sudokuGrid.lookup("#" + cellId);
                 if (cells[i][j] != null) {
-                    Listener.CellInfo cellInfo = new Listener.CellInfo(cells[i][j], i, j);
-                    Listener.addTextListener(cellInfo);
+                    cellInfos[i][j] = new Listener.CellInfo(cells[i][j], i, j);
+                    Listener.addTextListener(cellInfos[i][j]);
                 }
+                contador++;
             }
         }
     }
@@ -133,8 +63,7 @@ public class GameController {
                 if (sudokuBoard.getCell(i, j) != 0) {
                     cells[i][j].setText(String.valueOf(sudokuBoard.getCell(i, j)));
                     cells[i][j].setStyle("-fx-control-inner-background:  #ADD8E6  ;");
-
-                    //cells[i][j].setEditable(false);
+                    Listener.removeTextListener(cellInfos[i][j]);
                 } else {
                     cells[i][j].setText("");
                 }
@@ -146,14 +75,11 @@ public class GameController {
         this.view = view;
     }
 
-
     @FXML
     public void handleButtonClick(ActionEvent event) {
-
         sudokuLogic.setBoard(sudokuBoard);
         sudokuBoard = sudokuLogic.randomClue();
     
     }
-
 
 }
